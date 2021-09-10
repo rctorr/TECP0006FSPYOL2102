7c4bc767def8e9eac041ee4afdc33bddaf5fa8b7
Ricardo Torres <rictor@cuhrt.com>
2021-09-10 15:47:49 -0500

    Actualizando archivos para Sesion-07

--- Sesion-07/Ejemplo-05/formulario.py
+++
@@ -1,8 +0,0 @@
-from wtforms import Form 
-from wtforms import StringField, TextField
-from wtforms.fields.html5 import EmailField
-
-class Formulario_usuario(Form):
-    usuario = StringField("Usuario")
-    correo = EmailField ("Correo electrónico")
-    comentario = TextField("Comentario")
